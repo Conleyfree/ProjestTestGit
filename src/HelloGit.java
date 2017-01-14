d35146b7e3766391cf32860a7b1ac58589aa0b87
HandsomeMrChen <HandsomeMrChen@163.com>
2017-01-14 21:54:08 +0800

    test commit

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,8 +1,14 @@
+import java.util.concurrent.SynchronousQueue;
+
 /**
  * Created by HandsomeMrChen on 2017/1/14.
  */
 public class HelloGit {
     public static void main(String[] args){
         System.out.println("Hello Git");
+    }
+
+    void testCommit(){
+        System.out.println("@_@");
     }
 }
