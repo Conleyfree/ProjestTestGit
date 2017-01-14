1fec104a1a07d6c1ece70418624429b26c29464b
HandsomeMrChen <HandsomeMrChen@163.com>
2017-01-14 23:41:36 +0800

    test commit

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -6,9 +6,11 @@ import java.util.concurrent.SynchronousQueue;
 public class HelloGit {
     public static void main(String[] args){
         System.out.println("Hello Git");
+        testCommit();
+
     }
 
-    void testCommit(){
+    static void testCommit(){
         System.out.println("@_@");
     }
 }
